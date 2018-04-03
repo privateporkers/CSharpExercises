4d6537afd0db2c8c5ee4be38d886b06cbf95b672
Cody Winton <cody.t.winton@gmail.com>
2018-04-03 18:24:07 -0500

    Added initial implementation of LoggingKata

---
+++ LoggingKata/LoggingKata/TacoParser.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using log4net;
+
+namespace LoggingKata
+{
+    /// <summary>
+    /// Parses a POI file to locate all the TacoBells
+    /// </summary>
+    public class TacoParser
+    {
+        public TacoParser()
+        {
+
+        }
+
+        private static readonly ILog Logger =
+            LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public ITrackable Parse(string line)
+        {
+            //DO not fail if one record parsing fails, return null
+            return null; //TODO Implement
+        }
+    }
+}
